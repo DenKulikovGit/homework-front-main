391cdaee8502c71cecd9c2007f2b13161b979088
DenKulikov <deniskulikov95787@gmail.com>
2025-09-02 20:15:04 +0300

    HW4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,19 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string; // текущее значение
+	setCurrentText: React.Dispatch<React.SetStateAction<string>>; // функция обновления состояния
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		props.setCurrentText(event.currentTarget.value); // обновляем состояние
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+		<input
+			id={'hw04-input'}
+			type="text"
+			value={props.currentText}
+			onChange={onChangeHandler}
+		/>
+	);
 };
